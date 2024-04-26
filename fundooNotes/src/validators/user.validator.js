49f301ca471571567aa36d3b3c440977c904b184
varasheb <varkanthi456@gmail.com>
2024-04-26 17:47:33 +0530

    [Varshab] Feature. User Registration

--- fundooNotes/src/validators/user.validator.js
+++ fundooNotes/src/validators/user.validator.js
@@ -1,4 +1,6 @@
 import Joi from '@hapi/joi';
+import HttpStatus from 'http-status-codes';
+
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
@@ -11,10 +13,13 @@ export const newUserValidator = (req, res, next) => {
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: error.message
+    });
   } else {
     req.validatedBody = value;
-    next();
+    next()
   }
 };
 
